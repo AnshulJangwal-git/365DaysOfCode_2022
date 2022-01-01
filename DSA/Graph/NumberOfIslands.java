11718adb71fac7bd0556878ce95fdc79f298cd0c
anshul <anshuljangwal006@gmail.com>
2022-01-01 14:00:01 +0530

    up

--- DSA/Graph/NumberOfIslands.java
+++ DSA/Graph/NumberOfIslands.java
@@ -46,3 +46,35 @@ public class NumberOfIslands {
      }
 
 }
+
+//Leetcode200 Solution of Number Of Islands...
+
+// class Solution {
+//     public int numIslands(char[][] grid) {
+//         int countOfComps = 0 ;
+        
+//         for(int i = 0; i < grid.length; i++){
+//             for(int j = 0; j < grid[0].length; j++){
+//                 if(grid[i][j] == '1'){
+//                     countOfComps ++ ;
+//                     dfs(grid, i, j) ;
+//                 }
+//             }
+//         }
+//         return countOfComps ;
+//     }
+    
+//     public static void dfs(char[][] grid, int i, int j){
+//         if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0'){
+//             return ;
+//         }
+        
+//         grid[i][j] = '0' ; //marking of visited
+        
+//         dfs(grid, i - 1, j) ;
+//         dfs(grid, i, j + 1) ;
+//         dfs(grid, i + 1, j) ;
+//         dfs(grid, i, j - 1) ;
+        
+//     }
+// }
