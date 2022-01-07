1e9ed74bc22093bff767018b075c7c607f1121b8
anshul <anshuljangwal006@gmail.com>
2022-01-07 18:31:31 +0530

    up

--- AApractice/Main.java
+++ AApractice/Main.java
@@ -2,32 +2,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in) ;
-        int n = sc.nextInt() ;
-        int[] arr = new int[n] ;
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt() ;
-        }
+        String words[] = {"abc", "abcd", "abcde"} ;
 
-
-        int ans[] = pickEven(arr, n) ;
-    }
-
-    public static int[] pickEven(int[] arr, int n){
-        int ans[] = new int[n] ;
-        int idx = 0 ;
-        
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] % 2 == 0){
-                ans[idx] = arr[i] ;
-                idx ++ ;
+        for(String st : words){
+            System.out.print(st + " ") ;
+            System.out.println() ;
+            for(char ch : st.toCharArray()){
+                System.out.print(ch + " ") ;
+                System.out.println() ;
             }
         }
 
-        if(ans.length != 0){
-            return ans ;
-        }
-
-        return new int[0] ;
     }
 }
