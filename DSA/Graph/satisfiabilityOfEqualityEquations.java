acdc2a7b9359b8301da33430a26391b26189770a
anshul <anshuljangwal006@gmail.com>
2022-01-08 17:58:10 +0530

    up

--- DSA/Graph/satisfiabilityOfEqualityEquations.java
+++ DSA/Graph/satisfiabilityOfEqualityEquations.java
@@ -64,3 +64,83 @@ class satisfiabilityOfEqualityEquations {
     }
     
 }
+
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+
+//   public static void main(String[] args) throws Exception {
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     int n = Integer.parseInt(br.readLine());
+
+//     String[] arr = new String[n];
+//     for (int i = 0; i < n; i++) {
+//       arr[i] = br.readLine();
+//     }
+
+//     System.out.println(equationsPossible(arr));
+//   }
+
+//   public static boolean equationsPossible(String[] equations) {
+//     int[] parent = new int[26] ;
+//     int[] rank = new int[26] ;
+    
+//     for(int i = 0; i < parent.length; i++){
+//         parent[i] = i ;
+//         rank[i] = 1 ;
+//     }
+    
+//     for(String eq : equations){
+//         if(eq.charAt(1) == '='){
+//             union(eq.charAt(0) - 'a', eq.charAt(3) - 'a', parent, rank) ;
+//         }
+//     }
+    
+//     for(String eq : equations){
+//         if(eq.charAt(1) == '!'){
+//             int lx = find(eq.charAt(0) - 'a', parent) ;
+//             int ly = find(eq.charAt(3) - 'a', parent) ;
+            
+//             if(lx == ly){
+//                 return false ;
+//             }
+//         }
+//     }
+//     return true ;
+//   }
+  
+//   public static void union(int x, int y, int[] parent, int[] rank){
+//       int lx = find(x, parent) ;
+//       int ly = find(y, parent) ;
+      
+//       if(lx != ly){
+//           if(rank[lx] > rank[ly]){
+//               parent[ly] = lx ;
+              
+//           }else if(rank[ly] > rank[lx]){
+//               parent[lx] = ly ;
+              
+//           }else{
+//               parent[lx] = ly ;
+//               rank[ly] ++ ;
+              
+//           }
+//       }
+//   }
+  
+//   public static int find(int x, int parent[]){
+//     if(parent[x] == x){
+//         return x ;
+//     }
+    
+//     int ans = find(parent[x], parent) ;
+//     parent[x] = ans ; //path compression in DSU
+//     return ans ;
+//   }
+  
+// }
+
+
+
