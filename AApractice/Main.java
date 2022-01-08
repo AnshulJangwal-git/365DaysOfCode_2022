447928878074cb188e8c6e93e67a1ec44761b8d4
anshul <anshuljangwal006@gmail.com>
2022-01-08 17:27:34 +0530

    up

--- AApractice/Main.java
+++ AApractice/Main.java
@@ -2,16 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        String words[] = {"abc", "abcd", "abcde"} ;
-
-        for(String st : words){
-            System.out.print(st + " ") ;
-            System.out.println() ;
-            for(char ch : st.toCharArray()){
-                System.out.print(ch + " ") ;
-                System.out.println() ;
-            }
-        }
-
+        int a = 'g' - 'a' ;
+        System.out.println(a) ;
     }
 }
