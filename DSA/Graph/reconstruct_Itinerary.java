3b7445ef3ef95e1661f3fb2a6f7a545da0c33db8
anshul <anshuljangwal006@gmail.com>
2022-01-17 16:31:03 +0530

    up

--- DSA/Graph/reconstruct_Itinerary.java
+++ DSA/Graph/reconstruct_Itinerary.java
@@ -1,14 +1,56 @@
 //Leetcode 332 - https://leetcode.com/problems/reconstruct-itinerary/
 import java.util.* ;
-class Solutreconstruct_Itineraryion {
+// class reconstruct_Itinerary {
+//     HashMap<String, PriorityQueue<String>> graph ;
+//     LinkedList<String> ans ;
+//     Stack<String> st ;
+    
+//     public List<String> findItinerary(List<List<String>> tickets) {
+//         graph = new HashMap<>() ;
+//         ans = new LinkedList<>() ;
+//         st = new Stack<>() ;
+        
+//         for(List<String> ticket : tickets){
+//             PriorityQueue<String> temp = graph.getOrDefault(ticket.get(0), new PriorityQueue<>()) ;
+//             temp.add(ticket.get(1)) ;
+//             graph.put(ticket.get(0), temp) ;
+//         }
+        
+//         dfs("JFK") ;
+//         while(st.size() > 0){
+//             ans.add(st.pop()) ;
+//         }
+        
+//         return ans ;
+//     }
+    
+//     public void dfs(String src){
+//         PriorityQueue<String> nbrs = graph.get(src) ;
+        
+//         while(nbrs != null && nbrs.size() > 0){
+//             String nbr = nbrs.remove() ;
+//             dfs(nbr) ;
+//         }
+//         st.push(src) ;
+//         // ans.add(src) ;
+//     }
+// }
+
+
+
+
+
+
+
+class reconstruct_Itinerary {
     HashMap<String, PriorityQueue<String>> graph ;
     LinkedList<String> ans ;
-    Stack<String> st ;
+    // Stack<String> st ;
     
     public List<String> findItinerary(List<List<String>> tickets) {
         graph = new HashMap<>() ;
         ans = new LinkedList<>() ;
-        st = new Stack<>() ;
+        // st = new Stack<>() ;
         
         for(List<String> ticket : tickets){
             PriorityQueue<String> temp = graph.getOrDefault(ticket.get(0), new PriorityQueue<>()) ;
@@ -17,9 +59,9 @@ class Solutreconstruct_Itineraryion {
         }
         
         dfs("JFK") ;
-        while(st.size() > 0){
-            ans.add(st.pop()) ;
-        }
+        // while(st.size() > 0){
+        //     ans.add(st.pop()) ;
+        // }
         
         return ans ;
     }
@@ -31,7 +73,8 @@ class Solutreconstruct_Itineraryion {
             String nbr = nbrs.remove() ;
             dfs(nbr) ;
         }
-        st.push(src) ;
+        // st.push(src) ;
+        ans.addFirst(src) ;
         // ans.add(src) ;
     }
 }
