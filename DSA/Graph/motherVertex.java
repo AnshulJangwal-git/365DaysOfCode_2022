17c2e3807ddc318c65ea003c31fb41d76750fafd
anshul <anshuljangwal006@gmail.com>
2022-01-19 18:47:56 +0530

    up

--- DSA/Graph/motherVertex.java
+++ DSA/Graph/motherVertex.java
@@ -3,32 +3,32 @@
     import java.util.*;
     import java.lang.*;
     import java.io.*;
-    class GFG
-    {
-        public static void main(String[] args) throws IOException
-        {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int T = Integer.parseInt(br.readLine().trim());
-            while(T-->0)
-            {
-                String[] s = br.readLine().trim().split(" ");
-                int V = Integer.parseInt(s[0]);
-                int E = Integer.parseInt(s[1]);
-                ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-                for(int i = 0; i < V; i++)
-                    adj.add(i, new ArrayList<Integer>());
-                for(int i = 0; i < E; i++){
-                    String[] S = br.readLine().trim().split(" ");
-                    int u = Integer.parseInt(S[0]);
-                    int v = Integer.parseInt(S[1]);
-                    adj.get(u).add(v);
-                }
-                motherVertex obj = new motherVertex();
-                int ans = obj.findMotherVertex(V, adj);
-                System.out.println(ans);
-           }
-        }
-    }// } Driver Code Ends
+    // class GFG
+    // {
+    //     public static void main(String[] args) throws IOException
+    //     {
+    //         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //         int T = Integer.parseInt(br.readLine().trim());
+    //         while(T-->0)
+    //         {
+    //             String[] s = br.readLine().trim().split(" ");
+    //             int V = Integer.parseInt(s[0]);
+    //             int E = Integer.parseInt(s[1]);
+    //             ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+    //             for(int i = 0; i < V; i++)
+    //                 adj.add(i, new ArrayList<Integer>());
+    //             for(int i = 0; i < E; i++){
+    //                 String[] S = br.readLine().trim().split(" ");
+    //                 int u = Integer.parseInt(S[0]);
+    //                 int v = Integer.parseInt(S[1]);
+    //                 adj.get(u).add(v);
+    //             }
+    //             motherVertex obj = new motherVertex();
+    //             int ans = obj.findMotherVertex(V, adj);
+    //             System.out.println(ans);
+    //        }
+    //     }
+    // }// } Driver Code Ends
     
     
     
